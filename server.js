fab0c85a5c5a2cedd5d03cd9486e4f885479e738
tuhinarout <165285241+tuhinarout@users.noreply.github.com>
2025-06-21 15:44:49 +0530

    adding correct files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-const allowedOrigin = 'https://digitalwallet-tkg4.onrender.com'
+const allowedOrigin = 'https://digitalwallet-tkg4.onrender.com';
 
 app.use(cors({
   origin: allowedOrigin,
